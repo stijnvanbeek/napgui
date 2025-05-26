abdb4037d9d82163841e3896874997a69c88fef1
Stijn van Beek <stijn.v.beek@gmail.com>
2025-05-26 19:29:20 +0200

    Improve HorizontalLayout

--- src/Gui/HorizontalLayout.cpp
+++ src/Gui/HorizontalLayout.cpp
@@ -7,6 +7,8 @@
 RTTI_BEGIN_CLASS(nap::gui::HorizontalLayout)
 	RTTI_PROPERTY("ColumnWidths", &nap::gui::HorizontalLayout::mColumnWidths, nap::rtti::EPropertyMetaData::Default)
 	RTTI_PROPERTY("Borders", &nap::gui::HorizontalLayout::mBorders, nap::rtti::EPropertyMetaData::Default)
+	RTTI_PROPERTY("Resizable", &nap::gui::HorizontalLayout::mResizable, nap::rtti::EPropertyMetaData::Default)
+	RTTI_PROPERTY("RelativeSizes", &nap::gui::HorizontalLayout::mRelativeSizes, nap::rtti::EPropertyMetaData::Default)
 	RTTI_PROPERTY("Content", &nap::gui::HorizontalLayout::mContent, nap::rtti::EPropertyMetaData::Embedded)
 RTTI_END_CLASS
 
@@ -14,18 +16,67 @@ namespace nap
 {
 	namespace gui
 	{
+		bool HorizontalLayout::init(utility::ErrorState &errorState)
+		{
+			if (mColumnWidths.empty())
+				return true;
+
+			if (mColumnWidths.size() != mContent.size())
+			{
+				errorState.fail("Layout %s: ColumnWidths needs to be empty or equal size to Content");
+				return false;
+			}
+
+			return true;
+		}
+
 
 		void HorizontalLayout::draw()
 		{
-			auto guiID = "###" + mID;
-			ImGui::BeginColumns(guiID.c_str(), mContent.size());
-			for (auto i = 0; i < mContent.size(); ++i)
+			auto height = ImGui::GetContentRegionAvail().y;
+			auto totalWidth = ImGui::GetContentRegionAvail().x;
+			if (mColumnWidths.empty())
+				mColumnWidths.resize(mContent.size(), totalWidth / mContent.size());
+
+			// Normalize column widths
+			if (mRelativeSizes)
 			{
-				mContent[i]->show();
-				if (i < mContent.size() - 1)
-					ImGui::NextColumn();
+				auto sum = 0.f;
+				for (auto& column : mColumnWidths)
+					sum += column;
+				for (auto& column : mColumnWidths)
+					column = (column / sum) * totalWidth;
 			}
-			ImGui::EndColumns();
+
+			ImGuiWindowFlags flags = ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoScrollWithMouse | ImGuiWindowFlags_HorizontalScrollbar;
+
+			ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0,0));
+			auto i = 0;
+			for (auto& column : mContent)
+			{
+				std::string id = mID + "Column" + std::to_string(i);
+				ImGui::BeginChild(id.c_str(), ImVec2(mColumnWidths[i], height), mBorders, flags);
+				ImGui::PopStyleVar();
+				column->show();
+				ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0,0));
+				ImGui::EndChild();
+				if (column != mContent.back())
+				{
+					if (mResizable)
+					{
+						ImGui::SameLine();
+						ImGui::InvisibleButton((mID + "Splitter" + std::to_string(i)).c_str(), ImVec2(8.f, height));
+						if (ImGui::IsItemActive())
+						{
+							mColumnWidths[i] += ImGui::GetIO().MouseDelta.x;
+							mColumnWidths[i + 1] -= ImGui::GetIO().MouseDelta.x;
+						}
+					}
+					ImGui::SameLine();
+				}
+				++i;
+			}
+			ImGui::PopStyleVar();
 		}
 
 	}
--- src/Gui/HorizontalLayout.h
+++ src/Gui/HorizontalLayout.h
@@ -16,13 +16,17 @@ namespace nap
 		{
 			RTTI_ENABLE(Gui)
 		public:
-			std::vector<float> mColumnWidths; ///< Property: 'ColumnWidhts' The width of each column. This property can be left empty to fill up all available space with equal sized columns. Alternatively the size of the vector can be smaller than the number of columns the wrap around the values.
+			std::vector<float> mColumnWidths; ///< Property: 'ColumnWidhts' The width of each column. This property can be left empty to fill up all available space with equal sized columns. The sizes are normalized to fill up the entire available region if mRelativeSizes is true.
 
-			bool mBorders = false; ///< Property: 'Borders' True if borders should be drawn between the columns.
+			bool mBorders = true; ///< Property: 'Borders' True if borders should be drawn between the columns.
+			bool mResizable = true; ///< Property: 'Resizable' True if user can drag the column sizes
+			bool mRelativeSizes = true; ///< Property: 'RelativeSizes' Set to true to normalize mColumnWidths
 			std::vector<ResourcePtr<Gui>> mContent; ///< Property: 'Content' An embedded Gui to be displayed in each column. The size of this vector equals the number of columns.
 
 		public:
 			HorizontalLayout() = default;
+
+			bool init(utility::ErrorState &errorState) override;
 
 		private:
 			void draw() override;
